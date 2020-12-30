50dbd1f9b9449286fdeed67123d41d882add4745
YeFei <316606233@qq.com>
2020-12-30 19:40:17 +0800

    重写 logger, fail

--- example/app/index.js
+++ example/app/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { router } = require('../..');
+const { router, fail } = require('../..');
 
 router.get('/', ctx => {
   ctx.body = {
@@ -13,4 +13,10 @@ router.get('/name/:name', ctx => {
   ctx.body = {
     name: ctx.params.name,
   };
+});
+
+router.get('/error', ctx => {
+  // 错误输出终止执行
+  fail('error demo');
+  console.log('后续代码不会执行');
 });
--- index.js
+++ index.js
@@ -4,65 +4,106 @@ const Sentry = require('@sentry/node');
 const Koa = require('koa');
 const Router = require('@koa/router');
 const bodyParser = require('koa-bodyparser');
-const logger = require('koa-pino-logger');
+const pino = require('pino');
 const globby = require('globby');
 const fs = require('fs');
 const path = require('path');
-
-// Sentry
-if (process.env.SENTRY_DSN) {
-  console.log('SENTRY_DSN:', process.env.SENTRY_DSN);
-  Sentry.init({ dsn: process.env.SENTRY_DSN });
-  app.on('error', (err, ctx) => {
-    Sentry.withScope(scope => {
-      scope.addEventProcessor(event => Sentry.Handlers.parseRequest(event, ctx.request));
-      Sentry.captureException(err);
-    });
-  });
-}
+const createError = require('http-errors');
+const FAIL_BODY = Symbol('FAIL_BODY');
 
 // Koa
 const app = new Koa();
 const router = new Router();
 
-app.use(logger({
-  level: process.env.LOGGER_LEVEL || 'warn',
-}));
+// logger
+const logger = pino();
+app.use(function(ctx, next) {
+  ctx.log = logger.child({
+    method: ctx.method,
+    url: ctx.url,
+    host: ctx.host,
+    ip: ctx.ip,
+  });
+  return next();
+});
 
-app.use(bodyParser());
+// Sentry
+if (process.env.SENTRY_DSN) {
+  logger.info('SENTRY_DSN: %s', process.env.SENTRY_DSN);
+  Sentry.init({ dsn: process.env.SENTRY_DSN });
+  app.on('error', (err, ctx) => {
+    if (ctx) {
+      Sentry.withScope(scope => {
+        scope.addEventProcessor(event => Sentry.Handlers.parseRequest(event, ctx.request));
+        Sentry.captureException(err);
+      });
+    } else {
+      Sentry.captureException(err);
+    }
+  });
+}
+
+// 记录错误信息
+app.on('error', (err, ctx) => {
+  if (ctx) {
+    ctx.log.error(err);
+  } else {
+    logger.error(err);
+  }
+});
 
 /**
  * 统一返回错误信息
- * 
- * @param {Koa.ParameterizedContext} ctx 
- * @param {string} msg 错误消息
- * @param {any} [data] 附加数据
+ * @param {string} message 错误消息
  * @param {number} [code=1] 错误代码
+ * @param {any} [data] 附加数据
  */
-function fail(ctx, msg, data, code = 1) {
-  ctx.log.warn('fail[%s]: %s %s', code, msg, data);
-  ctx.status = 422;
-  ctx.body = {
+function fail(message, code = 1, data) {
+  const err = createError(422, message);
+  err[FAIL_BODY] = {
     code,
-    msg,
+    message,
     data,
   };
+  throw err;
 }
 
 /**
- * 加载 app/ 文件夹内的文件
+ * 自定义错误处理
+ * @param {Error} err 
  */
-function load() {
-  const directory = path.join(process.cwd(), 'app');
+app.context.onerror = function(err) {
+  if (null == err) return;
+  if (!err.expose) {
+    this.app.emit('error', err, this);
+  }
+  this.status = err.status || 500;
+  this.type = 'json';
+  this.body = JSON.stringify(err[FAIL_BODY] || this.body || { code: 1, message: err.message });
+  this.length = Buffer.byteLength(this.body);
+  this.res.end(this.body);
+};
+
+// POST 内容解析
+app.use(bodyParser());
+
+/**
+ * 加载目录内的js文件
+ * @param {string} directory 目标目录
+ */
+function load(directory) {
+  let count = 0;
   for (const filepath of globby.sync('**/*.js', { cwd: directory })) {
     const fullpath = path.join(directory, filepath);
     if (!fs.statSync(fullpath).isFile()) continue;
     try {
       require(fullpath);
+      count++;
     } catch (e) {
-      console.error('load error:', filepath, e);
+      console.error('Load error: %s, %s', fullpath, e.message);
     }
   }
+  console.log('Loaded: %s %o files', directory, count);
 }
 
 /**
@@ -71,19 +112,24 @@ function load() {
  */
 function start(port = 7001) {
   const startTime = Date.now();
-  load();
+  const directory = path.join(process.cwd(), 'app');
+  if (fs.existsSync(directory)) {
+    load(directory);
+  }
   // 打印出已经加载的路由
   router.stack.forEach(i => console.log('Router: [%s] %s', i.methods.join(','), i.path));
   app.use(router.routes());
   app.use(router.allowedMethods());
   app.listen(port, () => {
-    console.log(`Server on %s. %oms`, port, Date.now() - startTime);
+    console.log(`Server: %s. %o ms`, port, Date.now() - startTime);
   });
 }
 
 module.exports = {
   app,
   router,
+  logger,
+  load,
   fail,
   start,
 };
