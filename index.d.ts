c59ddc331d8dbc5a850ac11a60b1bdc4883fcde5
YeFei <316606233@qq.com>
2021-03-01 15:26:49 +0800

    1.3.1

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,7 @@ import '@zenweb/helper';
 import '@zenweb/service';
 export { Core } from '@zenweb/core';
 export { ApiFail, ApiFailDetail } from '@zenweb/api';
+export { Service } from '@zenweb/service';
 import { ApiOptions } from '@zenweb/api';
 import { Core } from '@zenweb/core';
 import { Options as BodyOptions } from '@zenweb/body';
